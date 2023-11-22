c5308333e79d3b3fe15350784db171097da5df28
Bartosz Butrym <butrymbartosz@gmail.com>
2023-11-22 16:41:30 +0100

    implement basic structure, add router and styling libraries

---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Card } from 'react-bootstrap';
+
+const Product = ({ product }) => {
+	return (
+		<Card className='my-3 p-3 rounded'>
+			<Link to={`/product/${product._id}`}>
+				<Card.Img src={product.image} variant='top' />
+			</Link>
+
+			<Card.Body>
+				<Link to={`/product/${product._id}`}>
+					<Card.Title as='div'>
+						<strong>{product.name}</strong>
+					</Card.Title>
+				</Link>
+
+				<Card.Text as='h3'>${product.price}</Card.Text>
+			</Card.Body>
+		</Card>
+	);
+};
+
+export default Product;
+
+Product.propTypes = {
+	product: PropTypes.shape({
+		_id: PropTypes.string,
+		name: PropTypes.string,
+		image: PropTypes.string,
+		price: PropTypes.number,
+	}),
+};
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import HomeScreen from './screens/HomeScreen.jsx';
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
+
+export const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		errorElement: (
+			<div>
+				<h3>Something went wrong.</h3>
+			</div>
+		),
+		children: [
+			{
+				index: true,
+				element: <HomeScreen />,
+			},
+			{
+				path: '*',
+				element: (
+					<div>
+						<h2>Not Found!</h2>
+					</div>
+				),
+			},
+		],
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
